3d49a83c926e8b3f16784a5348992643fc7a80aa
Gwgjnr <geoffwgg@gmail.com>
2021-12-29 12:38:18 +0000

    add event modal and make migration

--- board/models.py
+++ board/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    summary = models.TextField(blank=True)
+    details = models.TextField()
+    requirements = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    signed_up = models.ManyToManyField(
+        User, related_name='event_numbers', blank=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return self.title
+
+    def number_of_sign_ups(self):
+        return self.signed_up.count()
