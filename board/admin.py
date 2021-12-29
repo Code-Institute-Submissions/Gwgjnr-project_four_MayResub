34740a10d2f57058cc69183737413ad9c8a9937e
Gwgjnr <geoffwgg@gmail.com>
2021-12-29 13:14:56 +0000

    add summernote to allow styling of events

--- board/admin.py
+++ board/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Event
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+@admin.register(Event)
+class PostAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('details', 'requirements')
